727186b8299a8268d6fc9a554a3e4952f134f677
thomas <thomas.bariabas@gmail.com>
2020-07-29 23:40:01 +0300

    Fix Production build

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -3,12 +3,14 @@ import Init, {app} from './initializeServer'
 import InitRoutes from '../routes'
 import InitializePassport from './initializePassport'
 
-const DIST_DIR = __dirname
 
-app.use(express.static(DIST_DIR))
 Init()
 InitRoutes()
 InitializePassport()
+
+const DIST_DIR = __dirname
+app.use(express.static(DIST_DIR))
+
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
