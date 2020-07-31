8d638143eb5235005e63823e1e8bfc6d9ea31c47
thomas <thomas.bariabas@gmail.com>
2020-07-31 16:23:41 +0300

    Change the posts repository

--- src/server/initializeServer.js
+++ src/server/initializeServer.js
@@ -5,12 +5,11 @@ import bodyParser from 'body-parser'
 import passport from 'passport'
 export let app = null
 export default ()=>{
-    app.use(cors())
     app = express()
     connectToDb()
+    app.use(cors())
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use(bodyParser.raw());
     app.use(passport.initialize());
-    app.use(cors())
 }
