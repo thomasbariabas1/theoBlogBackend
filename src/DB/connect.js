15ff9c69a7a991a979f248d0e9bc8186afaccce4
thomas <thomas.bariabas@gmail.com>
2020-08-05 12:40:07 +0300

    DB

--- src/DB/connect.js
+++ src/DB/connect.js
@@ -1,7 +1,13 @@
 import mongoose  from 'mongoose'
+const dbUsername = process.env.DB_USERNAME
+const dbPassword = process.env.DB_PASSWORD
+const dbHost = process.env.DB_HOST
+const dbPort = process.env.DB_PORT
+const databaseName = process.env.DATABASE_NAME
+
 
 export const connectToDb = () => {
-    mongoose.connect('mongodb://mondbUser:mongoDbpwd12345@18.189.32.129:27017/theoBlog', {useNewUrlParser: true});
+    mongoose.connect(`mongodb://${dbUsername}:${dbPassword}@${dbHost}:${dbPort}/${databaseName}`, {useNewUrlParser: true});
     // mongoose.connect('mongodb://localhost:27017/theoBlog', {useNewUrlParser: true});
     mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
     mongoose.connection.once('open', function() {
