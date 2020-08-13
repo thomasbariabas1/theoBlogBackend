0b4ce3080b8aff294abb93e43d56ed788911572b
thomas <thomas.bariabas@gmail.com>
2020-08-13 19:20:33 +0300

    Prod build

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -14,7 +14,7 @@ const DIST_DIR = __dirname
 app.use(express.static(DIST_DIR))
 
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`App listening to ${PORT}....`)
     console.log('Press Ctrl+C to quit.')
